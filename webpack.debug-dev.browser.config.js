404a3c0dad85096b5249fcb87d27daa1899c582d
dennisat <dennis@anel.co>
2021-12-29 08:37:39 +0100

    Maintenance: V2, Boilerplate and deps upgrade

--- webpack.debug-dev.browser.config.js
+++ webpack.debug-dev.browser.config.js
@@ -4,21 +4,31 @@ const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
 
-const loaders = require('./webpack.loaders');
+const rules = require('./webpack.loaders');
 const plugins = require('./webpack.plugins');
 
+const serverPort = Number(process.argv[process.argv.length - 1]);
+
+console.log('To debug open address: http://localhost:' + serverPort + ' on any browser');
+console.log('');
+
 const config = {
+  mode: "development",
   target: 'web', // help: https://webpack.github.io/docs/configuration.html#target
   entry: [
-    // inject some code in order to enable the auto refresh of the browse in case of a file's change
-	  'babel-polyfill',
-    'webpack-dev-server/client?http://localhost:8027',
-    // the entry application code
-    path.resolve(__dirname, 'dev/index.ts')
+    'webpack-dev-server/client?http://localhost:' + serverPort, // bundle the client for webpack-dev-server and connect to the provided endpoint
+    'webpack/hot/only-dev-server',                              // bundle the client for hot reloading, only- means to only hot reload for successful updates
+    path.resolve(__dirname, 'src/webpack-debug-browser.index.ts')
   ],
-	externals: [],
+  optimization: {
+    usedExports: true,       // true to remove the dead code, for more https://webpack.js.org/guides/tree-shaking/
+  },
+  devtool: "source-map",     // help: https://webpack.js.org/configuration/devtool/
+  devServer: {
+    port: serverPort,
+  },
+  externals: [],
   output: {
-    path: path.resolve(__dirname, 'debug-ground/debug-dev-on-browser'),
     filename: 'debug-dev-browser.js'
   },
   resolve: {
@@ -26,15 +36,11 @@ const config = {
     extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js", ".jsx"]
   },
   module: {
-    loaders: loaders
+    rules,
   },
-  node: {
-    // universal app? place here your conditional imports for node env
-    fs: "empty",
-    path: "empty",
-    child_process: "empty",
-  },
-  plugins: plugins,
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),     // enable HMR globally
+  ].concat(plugins),
 };
 
 module.exports = config;
